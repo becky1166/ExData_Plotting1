c562d9f69f61d01342a617b7102723aa612539bb
Rebecca Hoath <rebecca.hoath@googlemail.com>
2014-06-08 17:16:41 +0100

    plot 2

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@ plot1<-function()
     #read data, subset & format time & date
     allData<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
     data<-subset(allData, Date == "1/2/2007" | Date == "2/2/2007")
-    data$Time <- format(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+    data$Time <- strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
     data$Date<-as.Date(data$Date,format="%d/%m/%Y")
     
     #open png
---
+++ plot3.r
@@ -0,0 +1,18 @@
+plot3<-function()
+{
+    #read data, subset & format time & date
+    allData<-read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
+    data<-subset(allData, Date == "1/2/2007" | Date == "2/2/2007")
+    data$Time <- strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+    data$Date<-as.Date(data$Date,format="%d/%m/%Y")
+    
+    #open png
+    png(filename="plot2.png",width=480,height=480,units="px")
+    
+    #create graph
+    plot(data$Time,data$Global_active_power,type="n",xlab="",ylab="Global Active Power (kilowatts)")
+    lines(data$Time,data$Global_active_power)
+    
+    #close png
+    dev.off()
+}
